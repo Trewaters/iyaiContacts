0ce17a30f718b0ba08511706c201eddea6396de9
Tre' Grisby <trewaters@hotmail.com>
2019-05-02 12:06:57 -0700

    [REFACTORED] save zoho to db
    I had to refactor this code. I was nesting functions to deep. It was causing binding issues. I fixed it and mapped the fields properly. Some of the fields were empty since they did not match the mongoose data model. Looks good after testing.

--- src/app/Components/import-zoho/import-zoho.component.ts
+++ src/app/Components/import-zoho/import-zoho.component.ts
@@ -18,7 +18,7 @@ export class ImportZohoComponent implements OnInit {
   data: any;
   unSafeHtml: string;
   vSafeHtml: string;
-  newHdr:any; // db save code
+  newHdr: any; // db save code
 
   constructor(private fb: FormBuilder, private sanitizer: DomSanitizer, private importService: CntMongoService) { }
 
@@ -26,15 +26,10 @@ export class ImportZohoComponent implements OnInit {
   }
 
   onSubmit() {
-
-    // REFACTOR BEGIN
     function convertStr2Arr(longString) {
-
       let linesCntData: string[] = [];
       let newLineAsArr = [];
-
       linesCntData = longString.split("\n" || "\n\r");
-
       for (let i = 0; i < linesCntData.length; i++) {
         let strLine = "";
         strLine = linesCntData[i];
@@ -52,126 +47,87 @@ export class ImportZohoComponent implements OnInit {
         })
         // console.log('convertStr2Arr() returns newLineAsArr...', newLineAsArr);
       }
-
       return newLineAsArr; //This has all the lines including header as index 0.
     }
-
-    function createContactObj(arrHeaders,arrData) {// db save code
+    function createContactObj(arrHeaders, arrData) {
       let newObj = {};
       let newHeader = '';
       // console.log("arrData...", arrData);
-      arrData.forEach(function (element, ind) {
-        newHeader = arrHeaders[ind]
-        // newObj[newHeader] = arrData[ind];
-        switch(newHeader){
+      arrData.forEach(function (element, ind,arrData) {
+        // console.log('element...',element, arrData[ind]);
+        newHeader = arrHeaders[ind];
+        switch (newHeader) {
           case 'First Name':
-          newObj['nmPref'] = arrData[ind]
-          break;
+            newObj['nmPref'] = element
+            break;
           case 'Title':
-          newObj['orgTitle'] = arrData[ind]
-          break;
+            newObj['orgTitle'] = element
+            break;
           case 'Account Name':
-          newObj['org'] = arrData[ind]
-          break;
+            newObj['org'] = element
+            break;
           case 'Department':
-          newObj['orgStatus'] = arrData[ind]
-          break;
+            newObj['orgStatus'] = element;
+            break;
           case 'Secondary Email':
-          newObj['emOther'] = arrData[ind]
-          break;
+            newObj['emOther'] = element;
+            break;
           case 'Email':
-          newObj['emPref'] = arrData[ind]
-          break;
+            newObj['emPref'] = element;
+            break;
           case 'Mobile':
-          newObj['phMobile'] = arrData[ind]
-          break;
+            newObj['phMobile'] = element;
+            break;
           case 'Home Phone':
-          newObj['phHome'] = arrData[ind]
-          break;
+            newObj['phHome'] = element;
+            break;
           case 'Other Phone':
-          newObj['phOff'] = arrData[ind]
-          break;
+            newObj['phOff'] = element;
+            break;
           case 'Fax':
-          newObj['phFax'] = arrData[ind]
-          break;
+            newObj['phFax'] = element;
+            break;
           case 'Phone':
-          newObj['phPref'] = arrData[ind]
-          break;
+            newObj['phPref'] = element;
+            break;
           case 'Assistant':
-          newObj['supName'] = arrData[ind]
-          break;
+            newObj['supName'] = element;
+            break;
           case 'Asst Phone':
-          newObj['supPhone'] = arrData[ind]
-          break;
+            newObj['supPhone'] = element;
+            break;
           case 'Description':
-          newObj['notes'] = arrData[ind]
-          break;
+            newObj['notes'] = element;
+            break;
           case 'Twitter':
-          newObj['Twitter'] = arrData[ind]
-          break;
+            newObj['Twitter'] = element;
+            break;
+          default:
+            newObj['nmFull'] = arrData[4] + " " + arrData[5]; // combine First and Last Name fields for an iyai Full Name
+            // console.log('new object nmFull added...', newObj['nmFull']);
         }
-      });
+      }
+      );
       // console.log("arrData...", arrData);
       // console.log("newHeader...", newHeader);
       // console.log("newObj...", newObj);
       return newObj;
     }
-    // END REFACTOR
-
     let that = this;
-
     function makeTable(cntData) {
-
-      // let linesCntData = [];
-      // linesCntData = cntData.split("\n" || "\n\r");
-      // let columnsCntData = [];
-      // let columnsCntData = convertStr2Arr(cntData); // takes long string and converts each new line to an array element, including headers
       let tableCntData = [];
       tableCntData = convertStr2Arr(cntData); // takes long string and converts each new line to an array element, including headers
       let htmlString = "";
-      /* for (let i = 0; i < linesCntData.length; i++) {
-        let strLine = "";
-        let newLineAsArr = [];
-        strLine = linesCntData[i];
-        if (i === 0) {
-          newLineAsArr = strLine.split(/","|,/); // capture headers
-          newHdr = newLineAsArr;// db save code
-        } else {
-          newLineAsArr = strLine.split(/(?<="),|,(?=")|,(?!\s)/); // split on commas that are not quoted or are followed by a space (\s)
-        }
-        // removing extra quotes
-        let testRegex = new RegExp('"', 'g')
-        newLineAsArr.forEach(function (element, ind) {
-          newLineAsArr[ind] = element.replace(testRegex, '');
-          // let testStr = newLineAsArr[ind];
-          // console.log("newLineAsArr[]...after remove...", testStr.indexOf('"'))
-        })
-        columnsCntData = Array.from(newLineAsArr);
-        // turn line array into object, then save to db
-        // this.importService.addContact(tableCntData);
-        console.log("columnsCntData[]...", columnsCntData);// db save code
-        // console.log("this.data...",this.data);
-        if (i != 0 && columnsCntData) {          // db save code
-          import2Db(columnsCntData);
-        }
-        tableCntData[i] = columnsCntData;
-      } */
-
       htmlString = '<table class="w3-table-all">';
-
       for (let i = 0; i < tableCntData.length; i++) {
-
         htmlString += `<tr>`;
-
         if (i === 0) {
           for (let j = 0; j < tableCntData[0].length; j++) {
             htmlString += `<th>${tableCntData[0][j]}</th>`;
           }
         } else {
-
-         that.newHdr = createContactObj(tableCntData[0],tableCntData[i]);
-         that.importService.addContact(that.newHdr);
-
+          that.newHdr = createContactObj(tableCntData[0], tableCntData[i]);
+          that.importService.addContact(that.newHdr);
           for (let j = 0; j < tableCntData[i].length; j++) {
             htmlString += `<td>${tableCntData[i][j]}</td>`;
           }
@@ -181,32 +137,22 @@ export class ImportZohoComponent implements OnInit {
       htmlString += "</table>";
       return htmlString;
     }
-
     this.unSafeHtml = makeTable(this.data);
     this.vSafeHtml = this.sanitizer.sanitize(SecurityContext.HTML, this.unSafeHtml);
   }
-
   onFileChange(event) {
     let reader = new FileReader();
-
     if (event.target.files && event.target.files.length) {
       const [file] = event.target.files;
       reader.readAsArrayBuffer(file)
-
       reader.onload = () => {
         this.buf = reader.result;
-
         // console.log("this.buf...",this.buf);
-
         function ab2str(xbuf) {
-
           // console.log("xbuf...",xbuf);
-
           let stringBuf = String.fromCharCode.apply(null, new Uint8Array(xbuf)); // I used encode 8 so that I got the proper results
           // return String.fromCharCode.apply(null, new Uint8Array(buf));
-
           // console.log("stringBuf...",stringBuf);
-
           return stringBuf;
         }
         // Zoho CRM exports are 44 fields wide, in a CSV format.
